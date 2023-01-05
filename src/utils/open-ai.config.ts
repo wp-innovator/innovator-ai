341f4583d5b68919f8c77d7753bd15d472607843
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2023-01-05 10:11:40 +0600

    Enhancement: Long content article generation support (#4)
    * Enhancement: Max token value increases to support long article
    
    * chore: Updated some npm packages

--- src/utils/open-ai.config.ts
+++ src/utils/open-ai.config.ts
@@ -12,7 +12,8 @@ export const openai = new OpenAIApi(configuration);
 export const defaultConfigTextGenerator = {
     model: 'text-davinci-003',
     temperature: 0.7,
-    max_tokens: 256,
+    max_tokens: 2049, // max-limit = 2049
     top_p: 1,
     best_of: 1,
+    echo: true,
 };
